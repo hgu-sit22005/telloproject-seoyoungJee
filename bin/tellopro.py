81bb231fb5d173b4ac2056c6a98653d0f242012c
seoyoungJee <21700705@handong.edu>
2019-11-21 18:12:09 +0900

    tello hw

--- bin/tellopro.py
+++ bin/tellopro.py
@@ -7,8 +7,16 @@ import TelloPro
 tello = Tello()
 
 command_lst = []
-command_lst.append(TelloPro.get_instance('takeoff'))
-command_lst.append(TelloPro.get_instance('land'))
+command_lst.append(TelloPro.get_instance('takeoff', -1))
+command_lst.append(TelloPro.get_instance('up', 50))
+command_lst.append(TelloPro.get_instance('down', 50))
+command_lst.append(TelloPro.get_instance('right', 50))
+command_lst.append(TelloPro.get_instance('left', 50))
+command_lst.append(TelloPro.get_instance('forward', 50))
+command_lst.append(TelloPro.get_instance('back', 50))
+command_lst.append(TelloPro.get_instance('cw', 50))
+command_lst.append(TelloPro.get_instance('ccw', 50))
+command_lst.append(TelloPro.get_instance('land',-1))
 
 for command in command_lst:
 	tello.send_command_instance(command)
