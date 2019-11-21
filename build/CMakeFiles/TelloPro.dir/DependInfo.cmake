d8dc86ca7d8dfa27fa4ac86e1c541ed7df78a92f
seoyoungJee <21700705@handong.edu>
2019-11-21 18:08:35 +0900

    tello hw

---
+++ build/CMakeFiles/TelloPro.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data/telloproject-seoyoungJee/src/back.cpp" "/data/telloproject-seoyoungJee/build/CMakeFiles/TelloPro.dir/src/back.cpp.o"
+  "/data/telloproject-seoyoungJee/src/ccw.cpp" "/data/telloproject-seoyoungJee/build/CMakeFiles/TelloPro.dir/src/ccw.cpp.o"
+  "/data/telloproject-seoyoungJee/src/cw.cpp" "/data/telloproject-seoyoungJee/build/CMakeFiles/TelloPro.dir/src/cw.cpp.o"
+  "/data/telloproject-seoyoungJee/src/down.cpp" "/data/telloproject-seoyoungJee/build/CMakeFiles/TelloPro.dir/src/down.cpp.o"
+  "/data/telloproject-seoyoungJee/src/forward.cpp" "/data/telloproject-seoyoungJee/build/CMakeFiles/TelloPro.dir/src/forward.cpp.o"
+  "/data/telloproject-seoyoungJee/src/land.cpp" "/data/telloproject-seoyoungJee/build/CMakeFiles/TelloPro.dir/src/land.cpp.o"
+  "/data/telloproject-seoyoungJee/src/left.cpp" "/data/telloproject-seoyoungJee/build/CMakeFiles/TelloPro.dir/src/left.cpp.o"
+  "/data/telloproject-seoyoungJee/src/python_interface.cpp" "/data/telloproject-seoyoungJee/build/CMakeFiles/TelloPro.dir/src/python_interface.cpp.o"
+  "/data/telloproject-seoyoungJee/src/right.cpp" "/data/telloproject-seoyoungJee/build/CMakeFiles/TelloPro.dir/src/right.cpp.o"
+  "/data/telloproject-seoyoungJee/src/takeoff.cpp" "/data/telloproject-seoyoungJee/build/CMakeFiles/TelloPro.dir/src/takeoff.cpp.o"
+  "/data/telloproject-seoyoungJee/src/up.cpp" "/data/telloproject-seoyoungJee/build/CMakeFiles/TelloPro.dir/src/up.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/python3.6m"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
